82bc49740ab0839da5513b45b02b66e0860ca2ec
Mateusz Szumny <mateusz.szumny@pl.abb.com>
2020-04-06 11:28:21 +0200

    initial commit

---
+++ Microservices.Training.Service1.Application/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Microservices.Training.Service1.Application
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            
+        }
+    }
+}
